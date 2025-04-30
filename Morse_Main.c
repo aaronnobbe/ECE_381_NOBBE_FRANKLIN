1108457f376595f14af598247faf8c630a11db03
Billy-Franklin <bzfranklin@outlook.com>
2025-04-30 11:59:26 -0500

    Update Morse_Main.c

--- Morse_Main.c
+++ Morse_Main.c
@@ -1,4 +1,4 @@
-/* USER CODE BEGIN Header */
+ /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file           : main.c
@@ -649,4 +649,4 @@ void assert_failed(uint8_t *file, uint32_t line)
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
+#endif /* USE_FULL_ASSERT */: /* USER CODE BEGIN Header */
